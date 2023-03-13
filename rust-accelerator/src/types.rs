4ba3e0d8445510d78083b7b34af664eaf675ce0a
Tomaž Hribernik <tomaz.hribernik@protonmail.com>
2023-03-13 16:06:28 +0100

    Replaced default hasher with a 32-bit version

--- rust-accelerator/src/types.rs
+++ rust-accelerator/src/types.rs
@@ -3,20 +3,24 @@ use alloc::sync::Arc;
 use linux_kernel_module::println;
 use linux_kernel_module::sync::Mutex;
 use hashbrown::HashMap;
+use hash32::{BuildHasherDefault, FnvHasher};
 
 use crate::ipv4;
 // use crate::ipv6;
 
+type FlowKeyHasher = BuildHasherDefault<FnvHasher>;
+
 pub struct RustAccelerator {
-    pub ipv4_flows: Mutex<HashMap<ipv4::FlowKey, Arc<ipv4::Flow>>>,
+    pub ipv4_flows: Mutex<HashMap<ipv4::FlowKey, Arc<ipv4::Flow>, FlowKeyHasher>>,
     // pub ipv6_flows: Mutex<HashMap<ipv6::FlowKey, Arc<ipv6::Flow>>>,
 }
 
 impl linux_kernel_module::KernelModule for RustAccelerator {
     fn init() -> linux_kernel_module::KernelResult<Self> {
         println!("Hello from Rust!");
+
         Ok(RustAccelerator {
-            ipv4_flows: Mutex::new(HashMap::new()),
+            ipv4_flows: Mutex::new(HashMap::default()),
             // ipv6_flows: Mutex::new(HashMap::new()),
         })
     }
